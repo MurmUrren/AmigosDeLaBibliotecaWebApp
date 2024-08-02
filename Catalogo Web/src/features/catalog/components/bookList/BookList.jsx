0284d4e039be286f60f35ff9342b3e7a1a5c2ab5
AlanMSR <124849751+AlanMSR@users.noreply.github.com>
2024-08-02 11:31:14 -0700

    hotfix

--- Catalogo Web/src/features/catalog/components/bookList/BookList.jsx
+++ Catalogo Web/src/features/catalog/components/bookList/BookList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { useBookList } from '../hooks/useBookList';
-import BookCover from './BookCover';
+import { useBookList } from '../../hooks/useBookList';
+import BookCover from '../bookCover/BookCover';
 
 const BookList = () => {
   const { books, getBook, removeBook, loading, error } = useBookList();
