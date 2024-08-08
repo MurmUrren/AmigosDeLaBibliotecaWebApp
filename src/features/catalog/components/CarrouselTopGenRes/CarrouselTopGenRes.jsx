fce4323c39a91a57b5b7a90fe7509c33379344ba
Roberto192 <isaakbks@gmail.com>
2024-08-08 09:28:48 -0700

    chore: Update CarrouselTopGenRes styles for alternating border colors

--- src/features/catalog/components/CarrouselTopGenRes/CarrouselTopGenRes.jsx
+++ src/features/catalog/components/CarrouselTopGenRes/CarrouselTopGenRes.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './CarrouselTopGenRes.css';
 import useGenres from "@hooks/useGenres";
 import CollectionCard from '@components/collectionCard/CollectionCard';
+import { useNavigate } from 'react-router-dom';
 
 const CarrouselTopGenRes = ({ id }) => {
     const genres = useGenres.getTopViews(id);
+    const navigate = useNavigate();
 
     return (
         <div className='Carrousel'>
@@ -15,9 +17,11 @@ const CarrouselTopGenRes = ({ id }) => {
                             title={genre.Title}
                             img={genre.Img}
                             genre={genre}
-                            onClick={() => {
+                            onClick={async () => {
+                                await useGenres.updateViews(genre.id);
                                 const title = genre.Title.replace(/\s+/g, '-').toLowerCase();
-                                navigate(`/collection/${title}/${genre.id}`);
+                                navigate(`/catalog/${genre.id}`);
+                                //navigate(`/collection/${title}/${genre.id}`);
                             }}
                         />
                     </div>
