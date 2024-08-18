0d5273335bb2e492f37e86c7293f1736b7c942fa
AlanMSR <124849751+AlanMSR@users.noreply.github.com>
2024-08-18 13:39:37 -0700

    affd

--- src/features/catalog/pages/bookManagementPage/components/barcodeScanner/BarcodeScanner.jsx
+++ src/features/catalog/pages/bookManagementPage/components/barcodeScanner/BarcodeScanner.jsx
@@ -16,7 +16,7 @@ const BarcodeScanner = ({ getScannerISBN }) => {
   // const deviceId = devices?.find((device) => device.kind === "videoinput")?.deviceId; 
   // const videoInputDevices = devices?.filter((device) => device.kind === "videoinput") || [];
   // const deviceId = videoInputDevices?.[1]?.deviceId || videoInputDevices?.[0]?.deviceId;
-  const deviceId = devices?.[3]?.deviceId || devices?.[2]?.deviceId || devices?.[1]?.deviceId || devices?.[0]?.deviceId;
+  const deviceId = devices?.[5]?.deviceId || devices?.[2]?.deviceId || devices?.[1]?.deviceId || devices?.[0]?.deviceId;
   const [result, setResult] = useState("");
   const [showVideoFeed, setShowVideoFeed] = useState(true);
   
