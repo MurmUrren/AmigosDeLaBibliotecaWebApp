01c4f9db5af7ea2ca7ee62c2fcd0a9a162a2794c
Roberto192 <isaakbks@gmail.com>
2024-08-31 08:46:31 -0700

    derpugger

--- q
+++
@@ -1,6 +0,0 @@
-  bookcard[m
-  generosb[m
-  genrep[m
-  genresb[m
-* [32mmain[m
-  order[m
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './app/App.jsx';
 import './app/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/AmigosDeLaBibliotecaWebApp">
+  <HashRouter basename="/AmigosDeLaBibliotecaWebApp">
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
 );
